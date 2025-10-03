9b4c5d806a823427550ea7c9d3be8a9c531351ef
DeveloperFromIndia <gnchrv.dll@gmail.com>
2025-10-04 01:39:43 +0200

    made something beautiful

--- src/bot/features/installify.ts
+++ src/bot/features/installify.ts
@@ -11,6 +11,8 @@ export const setupInstallify = (bot: Bot<Context>) => {
         if (!info)
             return await ctx.reply("Video not found");
 
+        const msg = await ctx.reply("⏳ Saving video");
+
         switch (info.type) {
             case 'progressive':
                 let sizeP = info.format.filesize ?? info.format.filesize_approx ?? 0;
@@ -19,8 +21,14 @@ export const setupInstallify = (bot: Bot<Context>) => {
 
                 const sourceP = await installService.save(link, info.format);
                 const videoFileP = new InputFile(sourceP);
+                await ctx.api.editMessageText(
+                    msg.chat.id,
+                    msg.message_id,
+                    "⏳ Sending video"
+                );
                 await bot.api.sendVideo(ctx.chat.id, videoFileP);
                 await installService.remove(sourceP);
+                await ctx.api.deleteMessage(msg.chat.id, msg.message_id);
                 return;
             case 'dash':
                 const videoSize = info.video.filesize ?? info.video.filesize_approx ?? 0;
@@ -34,10 +42,16 @@ export const setupInstallify = (bot: Bot<Context>) => {
                     { url: info.video.url, format: info.video.format },    // video
                     { url: info.audio.url, format: info.audio.format }     // audio
                 );
-                
+
                 const videoFileD = new InputFile(sourceD);
+                await ctx.api.editMessageText(
+                    msg.chat.id,
+                    msg.message_id,
+                    "⏳ Sending video"
+                );
                 await bot.api.sendVideo(ctx.chat.id, videoFileD);
                 await installService.remove(sourceD);
+                await ctx.api.deleteMessage(msg.chat.id, msg.message_id);
                 break;
         }
 
